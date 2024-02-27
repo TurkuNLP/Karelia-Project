e61a650e2e339ee2d5339102e26dadc72d65b1cd
Jenna Kanerva <jmnybl@utu.fi>
2024-02-27 12:11:25 +0200

    minor changes (rename variables)

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -34,7 +34,7 @@ def fuzzy_match(gold, ann, threshold=0.75):
                 ann.remove(a)
     return final_pairs, gold, ann
 
-def micro_f(gold, ann, args):
+def micro_f(gold, pred, args):
     
     # How many of the empty gold fields are empty also in prediction
     empty_gold = 0 # how many of the gold fields are empty
@@ -45,16 +45,16 @@ def micro_f(gold, ann, args):
     false_negatives = 0 # gold entity not predicted
     true_positives = 0 # prediction in gold
 
-    for gold_story, ann_story in zip(gold, ann):
+    for gold_story, pred_story in zip(gold, pred):
     
-        assert gold_story["index"] == ann_story["index"]
-        assert gold_story["primary_person_id"] == ann_story["primary_person_id"]
+        assert gold_story["index"] == pred_story["index"]
+        assert gold_story["primary_person_id"] == pred_story["primary_person_id"]
         
         field_names = ["person_hobbies", "person_social_orgs", "spouse_hobbies", "spouse_social_orgs"]
         for field_name in field_names:
 
             gold_entities = set([normalize(e.strip()) for e in gold_story[field_name].split(",") if e.strip() != ""])
-            ann_entities = set([normalize(e.strip()) for e in ann_story[field_name].split(",") if e.strip() != ""])
+            pred_entities = set([normalize(e.strip()) for e in pred_story[field_name].split(",") if e.strip() != ""])
             
             
             #### entity level metrics ####
@@ -62,33 +62,33 @@ def micro_f(gold, ann, args):
             # gold annotations empty (all predictions are false positives)
             if len(gold_entities) == 0:
                 empty_gold += 1
-                if len(ann_entities) == 0:
+                if len(pred_entities) == 0:
                     empty_correct += 1
                 else:
-                    false_positives += len(ann_entities)
+                    false_positives += len(pred_entities)
                 continue # done!
                 
             # prediction empty (all gold are false negatives)
-            if len(ann_entities) == 0:
+            if len(pred_entities) == 0:
                 false_negatives += len(gold_entities)
                 continue # done!
             
             # true positives (exact match)
-            common = gold_entities & ann_entities
+            common = gold_entities & pred_entities
             true_positives += len(common)
             
             # ..what is left
             gold_entities = gold_entities - common
-            ann_entities = ann_entities - common
+            pred_entities = pred_entities - common
             
             # fuzzy match
             if args.fuzzy_threshold != 1.0:
-                fuzzy_pairs, gold_entities, ann_entities = fuzzy_match(gold_entities, ann_entities, threshold=args.fuzzy_threshold)
+                fuzzy_pairs, gold_entities, pred_entities = fuzzy_match(gold_entities, pred_entities, threshold=args.fuzzy_threshold)
                 #print("Fuzzy pairs:", len(fuzzy_pairs))
                 true_positives += len(fuzzy_pairs)
             
             # fuzzy match done, the rest are false negatives or false positives
-            false_positives += len(ann_entities)
+            false_positives += len(pred_entities)
             false_negatives += len(gold_entities)
 
             
@@ -110,12 +110,12 @@ def main(args):
 
     with open(args.gold, "rt", encoding="utf-8") as f:
         gold_data = json.load(f)
-    with open(args.annotation, "rt", encoding="utf-8") as f:
-        annotation_data = json.load(f)
+    with open(args.prediction, "rt", encoding="utf-8") as f:
+        prediction_data = json.load(f)
 
     print("Fuzzy threshold:", args.fuzzy_threshold)
 
-    micro_f(gold_data, annotation_data, args)
+    micro_f(gold_data, prediction_data, args)
     
     
 if __name__=="__main__":
@@ -123,7 +123,7 @@ if __name__=="__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--gold", default=None, required=True, help="Gold standard annotation file")
-    parser.add_argument("--annotation", default=None, required=True, help="Annotation file")
+    parser.add_argument("--prediction", default=None, required=True, help="Annotation file")
     parser.add_argument("--fuzzy-threshold", type=float, default=1.0, help="Threshold for fuzzy matches, Default: 1.0, use Exact Match [EM], which includes only very basic normalization [casing, spacing, punctuation] but not fuzzy matches")
     args = parser.parse_args()
     
